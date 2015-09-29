79570a56c6129c75e57198f573521dd2c710469f
Lior Shahverdi <liorshahverdi@gmail.com>
2015-09-29 16:59:02 -0400

    starting mongo data loader

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,1 +1,14 @@
-import pymongo
+import csv
+
+
+dataFile = open('resources/311_Service_Requests_from_2010_to_Present.csv')
+dataReader = csv.reader(dataFile)
+i = 0 
+column_names = []
+for row in dataReader:
+	if i == 0:
+		column_names = list(row)
+		print column_names
+		i+=1
+	#else:
+		#print(row)
--- app/server.py
+++ app/server.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, url_for
-import pandas as pd 
-import os
 
 app = Flask(__name__)
 
-@app.route("/index", methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
 	return render_template('index.html')
 
