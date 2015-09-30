38581c541cc66a285b8447876fe9ddd149c085e1
Lior Shahverdi <liorshahverdi@gmail.com>
2015-09-30 00:32:50 -0400

    mongo data store added

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,14 +1,37 @@
+from pymongo import MongoClient
 import csv
 
+client = MongoClient()
+db = client.complaints
 
-dataFile = open('resources/311_Service_Requests_from_2010_to_Present.csv')
-dataReader = csv.reader(dataFile)
-i = 0 
-column_names = []
+csv_dataFile = open('resources/311_Service_Requests_from_2010_to_Present.csv')
+dataReader = csv.DictReader(csv_dataFile)
+
+# Get our column headers
+i = 0
+headers = None
 for row in dataReader:
 	if i == 0:
-		column_names = list(row)
-		print column_names
-		i+=1
-	#else:
-		#print(row)
+		headers = list(row)
+		break
+
+for eachline in dataReader:
+	row = {}
+	for field in headers:
+		row[field] = eachline[field]
+	db.complaints.insert_one(row)
+print 'Done!'
+
+
+'''
+result = db.complaints.insert_one (
+		{
+			"address": {
+				"street": "2 Avenue",
+	            "zipcode": "10075",
+	            "building": "1480",
+	            "coord": [-73.9557413, 40.7720266]	
+			}
+		}
+	)
+'''
