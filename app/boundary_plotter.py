aff34958f46e903d7bfc78450d47284e5def2d15
Lior Shahverdi <liorshahverdi@gmail.com>
2015-10-05 00:40:51 -0400

    too many requests

--- app/boundary_plotter.py
+++ app/boundary_plotter.py
@@ -2,17 +2,79 @@ import shapefile
 from glob import glob
 import time
 import random
+import json
+from pymongo import MongoClient
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 def index():
+	#cursor = db.complaints.find({'Complaint Type': 'Blocked Driveway'})
 	nyc = grab_nyc_neighborhoods()
 	all_neighborhood_coords = {}
 	for i in range(len(nyc)):
-		my_points = nyc[i].shape.points
-		name = str(nyc[i].record[3])
-		print name
+		my_points = nyc[i].shape.points # extract that neighboorhods border coordinates
+		neighborhood_name = str(nyc[i].record[3]) # name of neighborhood
 		new_list = get_neighborhood_coords(my_points)
-		all_neighborhood_coords[name] = new_list
-	return render_template('index.html', all_neighborhood_coords=all_neighborhood_coords)
+		all_neighborhood_coords[neighborhood_name] = new_list
+	num_complaints_by_neighborhood = {}
+	bronx_dat = categorize_complaints_to_neighborhood("BRONX")
+	print 'finished bronx.'
+	staten_island_dat = categorize_complaints_to_neighborhood("STATEN ISLAND")
+	print 'finished staten island.'
+	queens_dat = categorize_complaints_to_neighborhood("QUEENS")
+	print 'finished queens.'
+	brooklyn_dat = categorize_complaints_to_neighborhood("BROOKLYN")
+	print 'finished brooklyn.'
+	manhattan_dat = categorize_complaints_to_neighborhood("MANHATTAN")
+	print 'finished manhattan.'
+	num_complaints_by_neighborhood = bronx_dat.copy()
+	num_complaints_by_neighborhood.update(staten_island_dat)
+	num_complaints_by_neighborhood.update(queens_dat)
+	num_complaints_by_neighborhood.update(brooklyn_dat)
+	num_complaints_by_neighborhood.update(manhattan_dat)
+	json.dump(num_complaints_by_neighborhood, open("neighborhood_data.txt",'w'))
+	print 'Done!'
+
+def categorize_complaints_to_neighborhood(borough):
+	local_temp = {}
+	client = MongoClient()
+	db = client.complaints
+	cursor = db.complaints.find({"Borough": borough})
+	geolocator = Nominatim()
+	n_len = 0
+	for complaint in cursor:
+		n_len += 1
+		incident_address = str(complaint['Incident Address'])
+		city = str(complaint['City'])
+		query = None
+		location = None
+		if incident_address != '':
+			query = incident_address +' '+ city
+		try:
+			location = geolocator.geocode(query)
+		except GeocoderTimedOut as error_message:
+			print ("Error: geocode failed on input %s with message %s"%(query, error_message))
+
+		if location != None:
+			neighborhood = location.address.split(', ')[2]
+			if neighborhood in local_temp:
+				local_temp[neighborhood]['Total Complaints'] += 1
+				if complaint['Complaint Type'] in local_temp[neighborhood]:
+					local_temp[neighborhood][complaint['Complaint Type']] += 1
+				else:
+					local_temp[neighborhood][complaint['Complaint Type']] = 1
+			else:
+				local_temp[neighborhood] = {}
+				local_temp[neighborhood]['Total Complaints'] = 1
+				type_str = str(complaint['Complaint Type'])
+				local_temp[neighborhood][type_str] = 1
+
+			print local_temp
+			#print '\n\n\n\n\n\n'
+			#print neighborhood_name
+			#print complaint
+	#print 'Done!'
+	return local_temp
 
 
 # This method transforms the shapefile from zillow and finds all the points for the 
--- app/server.py
+++ app/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for
+from pymongo import MongoClient
 import shapefile
 from glob import glob
 import time
 import random
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 app = Flask(__name__)
 
@@ -15,8 +18,23 @@ def index():
 		name = str(nyc[i].record[3])
 		new_list = get_neighborhood_coords(my_points)
 		all_neighborhood_coords[name] = new_list
-	return render_template('index.html', all_neighborhood_coords=all_neighborhood_coords)
 
+	neighborhood_data_list = {}
+
+	bronx_dat = categorize_complaints_to_neighborhood("BRONX")
+	'''staten_island_dat = categorize_complaints_to_neighborhood("STATEN ISLAND")
+	queens_dat = categorize_complaints_to_neighborhood("QUEENS")
+	brooklyn_dat = categorize_complaints_to_neighborhood("BROOKLYN")
+	manhattan_dat = categorize_complaints_to_neighborhood("MANHATTAN")'''
+
+	neighborhood_data_list = bronx_dat.copy()
+	'''
+	neighborhood_data_list.update(staten_island_dat)
+	neighborhood_data_list.update(queens_dat)
+	neighborhood_data_list.update(brooklyn_dat)
+	neighborhood_data_list.update(manhattan_dat)
+	'''
+	return render_template('index.html', all_neighborhood_coords=all_neighborhood_coords, neighborhood_data_list=neighborhood_data_list)
 
 # This method transforms the shapefile from zillow and finds all the points for the 
 # different neighborhoods around the city
@@ -40,5 +58,43 @@ def get_neighborhood_coords(my_points):
 		temp.append(point[0])
 		new_list.append(temp)
 	return new_list
+
+def categorize_complaints_to_neighborhood(borough):
+	local_temp = {}
+	client = MongoClient()
+	db = client.complaints
+	cursor = db.complaints.find({"Borough": borough})
+	geolocator = Nominatim()
+	n_len = 0
+	for complaint in cursor:
+		n_len += 1
+		incident_address = str(complaint['Incident Address'])
+		city = str(complaint['City'])
+		query = None
+		location = None
+		if incident_address != '':
+			query = incident_address +' '+ city
+		try:
+			location = geolocator.geocode(query)
+		except GeocoderTimedOut as error_message:
+			print ("Error: geocode failed on input %s with message %s"%(query, error_message))
+
+		if location != None:
+			neighborhood = location.address.split(', ')[2]
+			if neighborhood in local_temp:
+				local_temp[neighborhood]['Total Complaints'] += 1
+				if complaint['Complaint Type'] in local_temp[neighborhood]:
+					local_temp[neighborhood][complaint['Complaint Type']] += 1
+				else:
+					local_temp[neighborhood][complaint['Complaint Type']] = 1
+			else:
+				local_temp[neighborhood] = {}
+				local_temp[neighborhood]['Total Complaints'] = 1
+				type_str = str(complaint['Complaint Type'])
+				local_temp[neighborhood][type_str] = 1
+
+			print local_temp
+			print '\n\n\n\n\n\n'
+	return local_temp
 
 app.run(debug=True)
